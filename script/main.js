430b4d839d5d39a0e6386c7bf580bc624c53c2c0
BrishmanCL <brishmancl@gmail.com>
2025-02-14 18:47:26 -0500

    v1

--- script/main.js
+++ script/main.js
@@ -102,9 +102,9 @@ const animationTimeline = () => {
     )
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
+    .from(".idea-2", 1.3, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
+    .from(".idea-3", 1.3, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
       x: 10,
